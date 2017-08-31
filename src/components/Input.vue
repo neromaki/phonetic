e2577ec82c7760c55c38e598adce023e4e9be8cf
Shaun <neromaki@gmail.com>
2017-08-31 13:56:41 +0100

    wip: putting animation changes on hold, building for prod

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,20 +1,6 @@
 <template>
     <section class="phonetic-input">
-            <p>for when you can't remember
-                <span class="phonetic-input--wrap">
-                    <div>
-                        <ul>
-                            <li v-for="phoneme in phonemeOneList" style="color: white">{{phoneme.phonetic}}</li>
-                        </ul>
-                    </div>
-                    <!--
-                    from
-                    <ul v-for="phoneme in phonemeTwoList">
-                        <li style="color: white">{{phoneme.phonetic}}</li>
-                    </ul>
-                -->
-                </span>
-            </p>
+            <p>for when you can't remember <span class="phonetic-input--wrap"><span>{{ phonemeOne.phonetic }}</span> from <span>{{ phonemeTwo.phonetic }}</span></span></p>
             <label id="phonetic-input--label" for="phonetic-input">Type in what you're trying to say</label>
             <input type="text" id="phonetic-input" class="phonetic-input" v-model="input" ref="phoneticInput" @keyup="updateOutput()" placeholder="Type in what you're trying to say" aria-labelledby="phonetic-input--label" autofocus>
     </section>
@@ -80,18 +66,17 @@ export default {
       }
     },
     updatePhonemeLists() {
-      const phonemeListOneElement = document.querySelector('.phonetic-input--wrap > div:first-child > ul');
+    //
+    //   function animate() {
+    //     phonemeListOneElement.top
+    //   }
 
-      function animate() {
-        // phonemeListOneElement.top 
-      }
-
-      const animateList = new Promise((resolve, reject) => {
-        setInterval(animate, 10);
-      });
-      this.addPhonemeToLists();
-      this.phonemeOneList.shift();
-      this.phonemeTwoList.shift();
+    //   const animateList = new Promise((resolve, reject) => {
+    //     setInterval(animate, 10);
+    //   });
+    //   this.addPhonemeToLists();
+    //   this.phonemeOneList.shift();
+    //   this.phonemeTwoList.shift();
     },
     updatePhonetics() {
       this.phonemeOne = PhoneticAlphabet[random(0, 24)];
